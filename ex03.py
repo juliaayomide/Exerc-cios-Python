fc961ec5a0b4c51acfcbf921ba45982d70978006
Julia Ayomide <145088749+juliaayomide@users.noreply.github.com>
2026-05-14 22:47:57 -0300

    Add program to check if a number is positive, negative, or zero

---
+++ ex03.py
@@ -0,0 +1,9 @@
+print('Esse programa lê um número que informa se o número é positivo, negativo ou zero')
+num1 = int(input('digite o número: '))
+
+if num1 > 0:
+    print('esse número é positvo')
+elif num1 == 0:
+    print('esse número é o número zero')
+else:
+    print('esse número é negativo')
